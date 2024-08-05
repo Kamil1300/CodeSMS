343f0147033a6ff218f88d433aa5c7a66bb9b521
Shaikh Kamil <108180928+Kamil1300@users.noreply.github.com>
2024-08-05 15:00:16 +0530

    auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import { db } from "@/lib/db";
-
+import Button from "@/components/ui/Button";
 export default async function Home() {
 
   await db.set('hello','hello')
 
-  return <div className="text-red-500">Allahu Akbar</div>;
+  return (
+    <>
+  <div className="text-red-500">Allahu Akbar</div>
+  <Button variant='ghost'>Yes</Button>
+  </>
+  )
 }
