2f5da36519577ac0c999a2472e361365aa4d86e6
Shaikh Kamil <108180928+Kamil1300@users.noreply.github.com>
2024-08-10 15:16:08 +0530

    some small additions

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,4 +1,7 @@
+import { fetchRedis } from '@/helpers/redis'
 import { authOptions } from '@/lib/auth'
+import { messageArrayValidator } from '@/lib/validations/message'
+import { Radius } from 'lucide-react'
 import { getServerSession } from 'next-auth'
 import { notFound } from 'next/navigation'
 import { FC } from 'react'
@@ -10,7 +13,23 @@ interface PageProps {
 }
 
 async function getChatMessages(chatId : string){
-  
+  try {
+    const results: string[] = await fetchRedis(
+      'zrange',
+      `chat${chatId}:messages`,
+      0,
+      -1
+    )
+    const dbMessages = results.map((message) => JSON.parse(message) as Message)
+
+    const reversedDbMessages = dbMessages.reverse()
+
+    const messages = messageArrayValidator.parse(reversedDbMessages)  
+
+    return messages
+  } catch (error) {
+    notFound()
+  }
 }
 
 const page = async ({params}:PageProps) => {
