909e21284a4d89cf927fc43fcbb1be549ee9decc
Shaikh Kamil <108180928+Kamil1300@users.noreply.github.com>
2024-08-17 15:26:39 +0530

    adding Chat

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -1,13 +1,39 @@
-import { FC } from 'react'
+"use client"
+
+import { chatHrefConstructor } from '@/lib/utils'
+import { usePathname, useRouter } from 'next/navigation'
+import { FC, useEffect, useState } from 'react'
 
 interface SidebarChatlistProps {
     friends: User[]
+    sessionId : string
 }
 
-const SidebarChatlist: FC<SidebarChatlistProps> = ({friends}) => {
+const SidebarChatlist: FC<SidebarChatlistProps> = ({friends,sessionId}) => {
+  const router = useRouter()
+  const pathname = usePathname()
+  const [unseenMessages,setUnseenMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    if(pathname?.includes('chat')){
+      setUnseenMessages((prev)=>{
+        return prev.filter((msg) => !pathname.includes(msg.senderId))
+      })
+    }
+  },[pathname])
+
   return <ul role='list' className='max-h-[25rem] overflow-y-auto -mx-2 space-y-1'>
     {friends.sort().map((friend) => {
-        
+      const unseenMessagesCount = unseenMessages.filter((unseenMsg)=>{
+        return unseenMsg.senderId === friend.id
+      }).length
+
+        return <li key={friend.id}>
+          <a href={`/dashboard/chat/${chatHrefConstructor(
+            sessionId,
+            friend.id 
+          )}`}>Hello</a>
+        </li>
     })}
   </ul>
 }
