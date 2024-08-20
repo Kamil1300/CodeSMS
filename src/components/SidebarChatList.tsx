803b72735cc76353ba8daf71b7126360c19f1f34
Shaikh Kamil <108180928+Kamil1300@users.noreply.github.com>
2024-08-20 15:46:21 +0530

    add some code in chat section

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -32,7 +32,11 @@ const SidebarChatlist: FC<SidebarChatlistProps> = ({friends,sessionId}) => {
           <a href={`/dashboard/chat/${chatHrefConstructor(
             sessionId,
             friend.id 
-          )}`}>Hello</a>
+          )}`} className='text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'>{friend.name}
+          {unseenMessagesCount > 0 ? (
+            <div className='bg-indigo-600 font-medium text-xs text-white w-4 h-4 rounded-full flex justify-center items-center'>{unseenMessagesCount}</div>
+          ) : null}
+          </a>
         </li>
     })}
   </ul>
