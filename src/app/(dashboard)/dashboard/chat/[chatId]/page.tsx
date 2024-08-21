008e2c7147202556d58b6005055b13a16c336ada
Shaikh Kamil <108180928+Kamil1300@users.noreply.github.com>
2024-08-21 15:29:14 +0530

    making input and button of chat section

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,3 +1,5 @@
+import ChatInput from '@/components/ChatInput'
+import Messages from '@/components/Messages'
 import { fetchRedis } from '@/helpers/redis'
 import { authOptions } from '@/lib/auth'
 import { db } from '@/lib/db'
@@ -74,6 +76,8 @@ const page = async ({params}:PageProps) => {
         </div>
       </div>
     </div>
+    <Messages sessionId={session.user.id} initialMessages={initialMessages}/>
+    <ChatInput chatId={chatId} chatPartner={chatPartner}/>
   </div>
 }
 
