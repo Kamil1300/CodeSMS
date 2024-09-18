39a3d65781011b001960e2dcfb90008256a83e66
Shaikh Kamil <108180928+Kamil1300@users.noreply.github.com>
2024-09-18 16:11:57 +0530

    Adding real time features in Chat and Toast notification

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -106,7 +106,11 @@ const Layout = async ({ children }: LayoutProps) => {
             </nav>
 
         </div>
-        {children}
+
+        <aside className='max-h-screen container py-16 md:py-12 w-full'>
+            {children}
+        </aside>
+
     </div>
 }
 
