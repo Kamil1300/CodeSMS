32a1fec6b940aec947988de0c7a95ec5b4a68925
Shaikh Kamil <108180928+Kamil1300@users.noreply.github.com>
2024-09-24 15:38:13 +0530

    adding dashboard

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -5,7 +5,7 @@ export const Icons = {
     Logo:(props:LucideProps) =>(
         
         <div className="flex items-center justify-center font-mono font-bold">
-                            &lt;CodeSMS <Image width={18} src="https://img.icons8.com/?size=100&id=60000&format=png&color=000000" alt="" />/&gt;
+                            &lt;CodeSMS <Image width={18} src={"https://img.icons8.com/?size=100&id=60000&format=png&color=000000"} alt="" />/&gt;
                             </div>
                   
     ),
